cefaefb6a82048a8148a1a5a82d643bd5e15b5a6
Kazi Suny Al Jarif <jarif@iut-dhaka.edu>
2023-05-19 22:24:38 +0600

    Added Admin User List

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -31,6 +31,8 @@ import AdminRoute from './components/AdminRoute';
 import ProductListScreen from './screens/ProductListScreen';
 import ProductEditScreen from './screens/ProductEditScreen';
 import OrderListScreen from './screens/OrderListScreen';
+import UserListScreen from './screens/UserListScreen';
+import UserEditScreen from './screens/UserEditScreen';
 
 function App() {
   const { state, dispatch: ctxDispatch } = useContext(Store);
@@ -189,6 +191,15 @@ function App() {
               ></Route>
 
               <Route
+                path="/admin/users"
+                element={
+                  <AdminRoute>
+                    <UserListScreen />
+                  </AdminRoute>
+                }
+              ></Route>
+
+              <Route
                 path="/admin/products"
                 element={
                   <AdminRoute>
@@ -202,6 +213,15 @@ function App() {
                 element={
                   <AdminRoute>
                     <ProductEditScreen />
+                  </AdminRoute>
+                }
+              ></Route>
+
+              <Route
+                path="/admin/user/:id"
+                element={
+                  <AdminRoute>
+                    <UserEditScreen />
                   </AdminRoute>
                 }
               ></Route>
